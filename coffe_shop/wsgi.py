75bd7b7b7aaebe0a52c2daecbc12b178f61c91de
Fran <falcoba@innexus.ar>
2025-05-27 12:59:26 -0300

    [CoffeShop] - create env file

--- coffe_shop/wsgi.py
+++ coffe_shop/wsgi.py
@@ -1,19 +1,17 @@
 import os
 import sys
+from dotenv import load_dotenv  # ⬅️ AGREGADO
 
 # Añade el path de tu proyecto al PYTHONPATH
 project_home = '/home/franzAlcoba/CoffeShop'  # ajusta con tu ruta exacta
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Activa el entorno virtual
-# activate_env = '/home/franzAlcoba/CoffeShop/venv/bin/activate_this.py'  # ruta a tu activate_this.py
-# with open(activate_env) as f:
-#     exec(f.read(), {'__file__': activate_env})
-os.environ['DJANGO_ENV'] = 'production'
+# ⬇️ CARGAMOS EL ARCHIVO .env ANTES DE CUALQUIER OTRA COSA
+env_path = os.path.join(project_home, '.env')
+load_dotenv(env_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffe_shop.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-# This file is used to configure the WSGI application for your Django project.
