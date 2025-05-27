5bb1244aca678ea0fec10c4b27c3fb735f3d1d05
Fran <falcoba@innexus.ar>
2025-05-27 09:27:54 -0300

    [CoffeShop] - db connection

--- coffe_shop/wsgi.py
+++ coffe_shop/wsgi.py
@@ -7,9 +7,9 @@ if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
 # Activa el entorno virtual
-activate_env = '/home/franzAlcoba/CoffeShop/venv/bin/activate_this.py'  # ruta a tu activate_this.py
-with open(activate_env) as f:
-    exec(f.read(), {'__file__': activate_env})
+# activate_env = '/home/franzAlcoba/CoffeShop/venv/bin/activate_this.py'  # ruta a tu activate_this.py
+# with open(activate_env) as f:
+#     exec(f.read(), {'__file__': activate_env})
 os.environ['DJANGO_ENV'] = 'production'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffe_shop.settings')
